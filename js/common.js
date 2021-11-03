9b2e0e074fd602448a4985c8c08701d3bfb0645e
minimar0 <minimar0@nate.com>
2021-11-03 15:46:53 +0900

    aa

--- js/common.js
+++ js/common.js
@@ -10,6 +10,6 @@ function activeModal() {
 	});
 }
 $(function() {
-	$('#cancer1').load('https://minimar0.github.io/insurance/cancer.html #incCancer1');
-	$('#cancer2').load('https://minimar0.github.io/insurance/cancer.html #incCancer2');
+	$('#incCancer1').load('https://minimar0.github.io/insurance/cancer.html #incCancer1');
+	$('#incCancer2').load('https://minimar0.github.io/insurance/cancer.html #incCancer2');
 });
